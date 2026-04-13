85cde8c145a0f52a43b109804ddf861b17cdbbec
Guillaume Guerin <guillaume.guerin@fromearthtolife.net>
2026-04-13 17:54:17 +0200

    feat: update VSCode configurations and add environment file to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mister-puzzle/',
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
